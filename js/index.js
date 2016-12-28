f51826764885d087a54c782d096bd62ae4e8df19
vaarsha <varshar186@gmail.com>
2016-12-28 19:41:25 +0530

    updated function

--- js/index.js
+++ js/index.js
@@ -1,4 +1,25 @@
 function myfunction() {
-  document.getElementById("qt").innerHTML = "Berry";
+  //document.getElementById("qt").innerHTML = "Berry";
+    var quotes = [
+    {
+      content: "Do not dwell in the past, do not dream of the future, concentrate the mind on the present moment.", 
+      author: "Gautama Buddha"
+    }, {
+      content: "The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart.",
+      author: "Helen Keller"
+    }, {
+      content: "Simplicity is the ultimate sophistication.",
+      author: "Leonardo da Vinci"
+    }, {
+      content: "You can't cross the sea merely by standing and staring at the water.",
+      author: "Rabindranath Tagore"
+    }, {
+      content: "There is nothing either good or bad, but thinking makes it so.",
+      author: "William Shakespeare"
+    }
+  ];
+  var random = Math.floor(Math.random() * (quotes.length));
+  document.getElementById("qt").innerHTML = quotes[random].content;
+  document.getElementById("qa").innerHTML = quotes[random].author;
 }
 
